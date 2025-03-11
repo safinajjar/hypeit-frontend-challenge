8151c1fdb27f79dbf610744ee2bec6fa521eb997
Musti <safi.najjar1@gmail.com>
2025-03-11 17:31:14 +0100

    styles und co.

--- src/components/Festival.tsx
+++ src/components/Festival.tsx
@@ -4,10 +4,10 @@ import { Button, Card, Group, Image, Text } from '@mantine/core';
 // name, date, location, image
 
 export type FestivalProps = {
-  name: string;
-  date: string;
-  location: string;
-  imageURL: string;
+  name: string | null | undefined;
+  date: string | null | undefined;
+  location: string | null | undefined;
+  imageURL: string | null | undefined;
 };
 
 export const Festival: FC<FestivalProps> = ({
@@ -19,17 +19,17 @@ export const Festival: FC<FestivalProps> = ({
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Card.Section>
-        <Image src={imageURL} height={160} alt={name} />
+        <Image src={imageURL} height={160} alt={name || ''} />
       </Card.Section>
 
       <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>{name}</Text>
-        <Text fw={500}>{date}</Text>
+        <Text fw={500} size="sm">
+          {name}
+        </Text>
+        <Text size="sm">{date}</Text>
       </Group>
 
-      <Text size="sm" c="dimmed">
-        {location}
-      </Text>
+      <Text size="md">{location}</Text>
 
       <Button color="blue" fullWidth mt="md" radius="md">
         show more
--- src/pages/Festivals.tsx
+++ src/pages/Festivals.tsx
@@ -2,12 +2,16 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   Container,
+  Grid,
   LoadingOverlay,
+  Space,
   Stack,
   Text,
   TextInput,
 } from '@mantine/core';
-import { useQueryFestivals } from 'src/api';
+import { Fragment } from 'react/jsx-runtime';
+import { FestivalType, useQueryFestivals } from 'src/api';
+import { Festival } from 'src/components/Festival';
 
 export const Festivals: React.FC = () => {
   const { data: festivals, isLoading, error } = useQueryFestivals({});
@@ -27,18 +31,29 @@ export const Festivals: React.FC = () => {
   }
 
   return (
-    <Container className="content" style={{ marginBlock: '3em' }}>
+    <Container style={{ marginBlock: '3em' }}>
       <TextInput
         leftSectionPointerEvents="none"
         leftSection={<FontAwesomeIcon icon={faMagnifyingGlass} />}
         placeholder="Search festival by name"
       />
 
-      {festivals?.map((festival) => (
-        
-      ))}
+      <Space h="lg" />
 
-      <h1>Hello World</h1>
+      <Grid>
+        {festivals?.map((festival: FestivalType) => (
+          <Fragment key={festival.id}>
+            <Grid.Col span={4}>
+              <Festival
+                name={festival.name}
+                date={festival.start}
+                location={festival.location?.city}
+                imageURL={festival.image}
+              />
+            </Grid.Col>
+          </Fragment>
+        ))}
+      </Grid>
     </Container>
   );
 };
